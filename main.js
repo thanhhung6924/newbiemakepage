178f5ba37a2ee2d33b679f6d05c3b466b0e44113
HUng <thanhhung6924@gmail.com>
2024-06-26 15:04:46 +0700

    ok

--- main.js
+++ main.js
@@ -5,28 +5,44 @@ let isClickable = true;
 const clickDelay = 1500;
 
 $(".filter__gallary").click(function (event) {
-    if (!isClickable) return; // Ngăn chặn click nếu clickDelay đang hoạt động
-    isClickable = false; // Đặt isClickable thành false để bắt đầu delay
+  if (!isClickable) return; // Ngăn chặn click nếu clickDelay đang hoạt động
+  isClickable = false; // Đặt isClickable thành false để bắt đầu delay
 
-    const nextIndex = (currentIndex + 1) % pages.length; // Tính toán nextIndex và quay lại đầu nếu vượt quá số phần tử
+  const nextIndex = (currentIndex + 1) % pages.length; // Tính toán nextIndex và quay lại đầu nếu vượt quá số phần tử
 
-    $(".gallary__item.active__gallary").addClass("disappearonleft");
-    $(pages[currentIndex]).one("webkitAnimationEnd", function (event) {
-        this.classList.remove("disappearonleft", "active__gallary");
-        pages[nextIndex].classList.add("active__gallary");
-        currentIndex = nextIndex; // Cập nhật currentIndex thành nextIndex
-        isClickable = true; // Cho phép click lại sau khi animation kết thúc
-    });
+  $(".gallary__item.active__gallary").addClass("disappearonleft");
+  $(pages[currentIndex]).one("webkitAnimationEnd", function (event) {
+    this.classList.remove("disappearonleft", "active__gallary");
+    pages[nextIndex].classList.add("active__gallary");
+    currentIndex = nextIndex; // Cập nhật currentIndex thành nextIndex
+    isClickable = true; // Cho phép click lại sau khi animation kết thúc
+  });
 
-    pages[nextIndex].classList.add("appearonright");
-    $(pages[nextIndex]).one("webkitAnimationEnd", function (event) {
-        this.classList.remove("appearonright");
-    });
+  pages[nextIndex].classList.add("appearonright");
+  $(pages[nextIndex]).one("webkitAnimationEnd", function (event) {
+    this.classList.remove("appearonright");
+  });
 
-    setTimeout(() => {
-        isClickable = true; // Đảm bảo cho phép click sau khoảng delay
-    }, clickDelay);
+  setTimeout(() => {
+    isClickable = true; // Đảm bảo cho phép click sau khoảng delay
+  }, clickDelay);
 });
 
 // Khởi tạo active__gallary cho phần tử đầu tiên
 pages[currentIndex].classList.add("active__gallary");
+//////////////
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".panel");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".horizontalt",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    end: () => "+=" + document.querySelector(".horizontalt").offsetWidth,
+  },
+});
